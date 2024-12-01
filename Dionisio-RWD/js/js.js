f8391b980be05f2c63fa82eb355ff09134e28eb6
LeahRollaine <s.lrrdionisio@usm.edu.ph>
2024-12-02 00:21:11 +0800

    yes

---
+++ Dionisio-RWD/js/js.js
@@ -0,0 +1,15 @@
+const Furniture = document.querySelector('.Furniture');
+const navlinks = document.querySelector('.nav-links');
+let menuOpen = false;
+
+Furniture.addEventListener('click', () => {
+	if (menuOpen == false) {
+		navlinks.style.display = "block";
+		menuOpen = true;
+		
+	}
+	else if (menuOpen = true) {
+			navlinks.style.display = "none";
+			menuOpen = false;
+	}
+});
