bdad5b498bb23cb952fd4c5665a543f9e8a085c8
Vianalaureis Milcah Coronel <s.vmmcoronel@usm.edu.ph>
2024-12-10 22:00:59 +0800

    TECHNOBLADE

---
+++ IntroToComputing-Website-Project/script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
+
