caa4c1fc378fe09c4abf4cf43076b5814f919d7c
nthl17 <s.nfrmanzanares@usm.edu.ph>
2024-12-18 10:42:54 +0800

    update

--- IntroToComputing-Project-Website/js/script.js
+++ IntroToComputing-Project-Website/js/script.js
@@ -1,31 +1,6 @@
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').ocnlick = () =>{
     navbar.classList.toggle('active');
-    searchform.classList.remove('active');
-    cartItem.classList.remove('active');
 }
-
-let searchform = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () => {
-    searchform.classList.toggle('active');
-    cartItem.classList.remove('active');
-    navbar.classList.remove('active');
-};
-
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () => {
-    cartItem.classList.toggle('active');
-    searchform.classList.remove('active');
-    navbar.classList.remove('active');
-};
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchform.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
 
